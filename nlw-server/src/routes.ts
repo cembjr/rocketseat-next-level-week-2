a7b078814c358369be6e7e27bb7986540f45e8c0
Carlos Barreto <carlos_edujr@hotmail.com>
2020-08-06 19:53:24 -0300

    NLW - Dia 02

---
+++ nlw-server/src/routes.ts
@@ -0,0 +1,14 @@
+import express, { request } from 'express';
+
+import ClassesControler from './controllers/ClassesController';
+
+const routes = express.Router();
+
+const classesController = new ClassesControler();
+
+routes.get('/classes', classesController.index);
+routes.post('/classes', classesController.create);
+
+
+
+export default routes;
